12e868334c24520a5eb7b4f4449cd0a403f7e34f
Michael Wayne <michaelwaynewibisono@gmail.com>
2023-10-17 22:10:42 +0700

    first commit

--- os_linux/client.py
+++ os_linux/client.py
@@ -1,23 +1,24 @@
 import xmlrpc.client
 
-# Replace <SERVER_IP> with the IP address of the server (e.g., 192.168.1.95)
-server_ip = "192.168.1.95"
+while True:
+    try:
+        # Create an XML-RPC client proxy
+        proxy = xmlrpc.client.ServerProxy(f"http://192.168.1.95:8000")
 
-# Create an XML-RPC client proxy
-proxy = xmlrpc.client.ServerProxy(f"http://{server_ip}:8000")
 
-# Ask the user for the filename to request
-filename = input("Please enter the filename to request: ")
+        filename = input("Please enter the filename to request (or 'exit' to quit): ")
+        if filename.lower() == 'exit':
+            break
 
-try:
-    file_data = proxy.send_file(filename)
+        file_data = proxy.send_file(filename)
 
-    if file_data:
-        # Write the received data to a local file
-        with open(f"{filename}", "wb") as file:
-            file.write(file_data.data)
-        print(f"File '{filename}' has been received successfully.")
-    else:
-        print("File request failed.")
-except Exception as e:
-    print(f"Error: {e}")
+        if file_data:
+            # Write the received data to a local file
+            with open(f"{filename}", "wb") as file:
+                file.write(file_data.data)
+                print(f"File '{filename}' has been received successfully.")
+        else:
+            print("File request failed.")
+
+    except Exception as e:
+        print(f"Error: {e}")
